a9340e666953b34038654fd532e5cd39783b9e18
fragex.eth <ehlers@outson.com>
2023-10-08 18:03:33 +0200

    adding discover page

---
+++ client/src/components/Common/Header.tsx
@@ -0,0 +1,17 @@
+import Logo from "../../assets/logo.svg"
+import { BiSearch } from 'react-icons/bi';
+
+export default function Header(){
+    return (
+        <header className="flex justify-between p-5 items-center mb-14">
+            <img src={Logo}/>
+            <div className=" w-[320px] h-[45px]">
+                <input className="absolute w-[320px] h-[45px] pr-8 pl-12 bg-neutral-100 rounded-[40px]" placeholder="Search..." ></input>
+                <div className="absolute z-2 text-lg mt-3.5 ml-5">
+                    <BiSearch/>
+                </div>
+            </div>
+            <button className="text-white bg-black rounded-[40px] shadow w-40 h-[45px]">Connect</button>
+        </header>
+    )
+}
---
+++ client/src/views/Discover.tsx
@@ -0,0 +1,138 @@
+import exampleImg from "./../assets/exampleImg.png"
+import profileImg from "./../assets/profilPic.png"
+
+export default function DiscoverView(){
+    return (
+        <div className="text-center mt">
+            <h1>Discover</h1>
+            <div className=" content-start gap-7 justify-between p-7 mt-16  surveys flex flex-wrap  bg-red-50 border border-solid mt-11 border-white max-w-[1270px] rounded-[45px] ml-auto mr-auto">
+                <div className="card w-96 bg-white h-full rounded-[35px] ">
+                    <div className="top relative">
+                        <img src={exampleImg} alt="" className="w-full"/>
+                        <div className="flex justify-between px-3 -mt-[70px] items-end justify-end">
+                            <button className=" px-8 h-12 bg-white rounded-3xl shadow border border-black justify-center">Invest</button>
+                            <img src={profileImg}></img>
+                        </div>
+                    </div>
+                    <div className="btm p-5 text-left">
+                        <div className="title flex justify-between">
+                         <span className="text-green-950 font-semibold ">Lorem name arst</span>
+                         <span><span className="font-semibold text-lg">1200$</span> / 1500$</span>
+                        </div>
+                        <div className="loadingbarOS h-0.5 bg-neutral-400 rounded-full my-1 relative">
+                            <div className="loadingbarIS h-1 bg-black w-1/3 rounded-full -mt-[1px] absolute"></div>
+                        </div>
+                        <p className="text-base text-justify leading-5">
+                        Lorem ipsum dolor sit amet, consectetur adipiscg elit, sed do eiusmod tempor incididunt ut labore et dolore magna al
+                        </p>
+                    </div>
+                </div>
+                <div className="card w-96 bg-white h-full rounded-[35px]">
+                    <div className="top relative">
+                        <img src={exampleImg} alt="" className="w-full"/>
+                        <div className="flex justify-between px-3 -mt-[70px] items-end justify-end">
+                            <button className=" px-8 h-12 bg-white rounded-3xl shadow border border-black justify-center">Invest</button>
+                            <img src={profileImg}></img>
+                        </div>
+                    </div>
+                    <div className="btm p-5 text-left">
+                        <div className="title flex justify-between">
+                         <span className="text-green-950 font-semibold ">Lorem name arst</span>
+                         <span><span className="font-semibold text-lg">1200$</span> / 1500$</span>
+                        </div>
+                        <div className="loadingbarOS h-0.5 bg-neutral-400 rounded-full my-1 relative">
+                            <div className="loadingbarIS h-1 bg-black w-1/3 rounded-full -mt-[1px] absolute"></div>
+                        </div>
+                        <p className="text-base text-justify leading-5">
+                        Lorem ipsum dolor sit amet, consectetur adipiscg elit, sed do eiusmod tempor incididunt ut labore et dolore magna al
+                        </p>
+                    </div>
+                </div>
+                <div className="card w-96 bg-white h-full rounded-[35px]">
+                    <div className="top relative">
+                        <img src={exampleImg} alt="" className="w-full"/>
+                        <div className="flex justify-between px-3 -mt-[70px] items-end justify-end">
+                            <button className=" px-8 h-12 bg-white rounded-3xl shadow border border-black justify-center">Invest</button>
+                            <img src={profileImg}></img>
+                        </div>
+                    </div>
+                    <div className="btm p-5 text-left">
+                        <div className="title flex justify-between">
+                         <span className="text-green-950 font-semibold ">Lorem name arst</span>
+                         <span><span className="font-semibold text-lg">1200$</span> / 1500$</span>
+                        </div>
+                        <div className="loadingbarOS h-0.5 bg-neutral-400 rounded-full my-1 relative">
+                            <div className="loadingbarIS h-1 bg-black w-1/3 rounded-full -mt-[1px] absolute"></div>
+                        </div>
+                        <p className="text-base text-justify leading-5">
+                        Lorem ipsum dolor sit amet, consectetur adipiscg elit, sed do eiusmod tempor incididunt ut labore et dolore magna al
+                        </p>
+                    </div>
+                </div>
+                <div className="card w-96 bg-white h-full rounded-[35px]">
+                    <div className="top relative">
+                        <img src={exampleImg} alt="" className="w-full"/>
+                        <div className="flex justify-between px-3 -mt-[70px] items-end justify-end">
+                            <button className=" px-8 h-12 bg-white rounded-3xl shadow border border-black justify-center">Invest</button>
+                            <img src={profileImg}></img>
+                        </div>
+                    </div>
+                    <div className="btm p-5 text-left">
+                        <div className="title flex justify-between">
+                         <span className="text-green-950 font-semibold ">Lorem name arst</span>
+                         <span><span className="font-semibold text-lg">1200$</span> / 1500$</span>
+                        </div>
+                        <div className="loadingbarOS h-0.5 bg-neutral-400 rounded-full my-1 relative">
+                            <div className="loadingbarIS h-1 bg-black w-1/3 rounded-full -mt-[1px] absolute"></div>
+                        </div>
+                        <p className="text-base text-justify leading-5">
+                        Lorem ipsum dolor sit amet, consectetur adipiscg elit, sed do eiusmod tempor incididunt ut labore et dolore magna al
+                        </p>
+                    </div>
+                </div>
+                <div className="card w-96 bg-white h-full rounded-[35px]">
+                    <div className="top relative">
+                        <img src={exampleImg} alt="" className="w-full"/>
+                        <div className="flex justify-between px-3 -mt-[70px] items-end justify-end">
+                            <button className=" px-8 h-12 bg-white rounded-3xl shadow border border-black justify-center">Invest</button>
+                            <img src={profileImg}></img>
+                        </div>
+                    </div>
+                    <div className="btm p-5 text-left">
+                        <div className="title flex justify-between">
+                         <span className="text-green-950 font-semibold ">Lorem name arst</span>
+                         <span><span className="font-semibold text-lg">1200$</span> / 1500$</span>
+                        </div>
+                        <div className="loadingbarOS h-0.5 bg-neutral-400 rounded-full my-1 relative">
+                            <div className="loadingbarIS h-1 bg-black w-1/3 rounded-full -mt-[1px] absolute"></div>
+                        </div>
+                        <p className="text-base text-justify leading-5">
+                        Lorem ipsum dolor sit amet, consectetur adipiscg elit, sed do eiusmod tempor incididunt ut labore et dolore magna al
+                        </p>
+                    </div>
+                </div>
+                <div className="card w-96 bg-white h-full rounded-[35px]">
+                    <div className="top relative">
+                        <img src={exampleImg} alt="" className="w-full"/>
+                        <div className="flex justify-between px-3 -mt-[70px] items-end justify-end">
+                            <button className=" px-8 h-12 bg-white rounded-3xl shadow border border-black justify-center">Invest</button>
+                            <img src={profileImg}></img>
+                        </div>
+                    </div>
+                    <div className="btm p-5 text-left">
+                        <div className="title flex justify-between">
+                         <span className="text-green-950 font-semibold ">Lorem name arst</span>
+                         <span><span className="font-semibold text-lg">1200$</span> / 1500$</span>
+                        </div>
+                        <div className="loadingbarOS h-0.5 bg-neutral-400 rounded-full my-1 relative">
+                            <div className="loadingbarIS h-1 bg-black w-1/3 rounded-full -mt-[1px] absolute"></div>
+                        </div>
+                        <p className="text-base text-justify leading-5">
+                        Lorem ipsum dolor sit amet, consectetur adipiscg elit, sed do eiusmod tempor incididunt ut labore et dolore magna al
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
