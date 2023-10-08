aafd4585704604f2a33a3a0d55670873d0200e42
fragex.eth <ehlers@outson.com>
2023-10-08 23:28:28 +0200

    adding profile page

--- client/src/views/Project.tsx
+++ client/src/views/Project.tsx
@@ -14,6 +14,20 @@ export default function Project(){
                     <FaLinkedin/>
                     <FaWallet/>
                 </div>
+                <div className="flex justify-center">
+                    <span className="-mt-4 -mb-4 text-right w-[850px] text-green-950 text-xl"><span className="font-semibold text-2xl">1100$</span> / 1300$</span>
+                </div>
+                <div className="progress h-0.5 w-3/4 bg-gray-400  ml-auto mr-auto">
+                    <div className="inside h-1 bg-black w-3/4 -mt-[1px] rounded-[40px]"></div>
+                </div>
+                <div className="mx-auto w-96 flex justify-between gap-5">
+                    <div className="h-12 flex border border-black rounded-3xl ">
+                        <input className="bg-white h-full w-full  rounded-s-3xl  px-8" placeholder="10"></input>
+                        <div className="   bg-gray-50 h-full w-[75px] rounded-e-3xl flex items-center px-4">BUSD</div>
+                    </div>
+                    <button className="px-8 h-12 bg-white rounded-3xl shadow border border-black justify-center">Invest</button>
+                </div>
+                <p className="flex justify-center text-xs -mt-4">By continuing, you agree to our<a>Terms of Service</a> and acknowledge our <a>Privacy Policy</a></p>
                 <p className=" text-justify">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
