92af0f69fd531a2e8e0ea1a34b4e786a98d3c4df
fragex.eth <ehlers@outson.com>
2023-10-24 18:49:19 +0200

    adding warning

--- blockchain/src/mock/Stablecoin.sol
+++ blockchain/src/mock/Stablecoin.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Stablecoin is ERC20 {
     constructor() ERC20("Stablecoin", "STB") {}
     
-    //No modifier | testing
+    //No modifier | don't use in production!!
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
